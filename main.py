2ca6000b092d8e15be608faa505019f42f9f05b6
Brij Patel <brij2608@gmail.com>
2026-07-29 15:45:12 +0530

    new

--- main.py
+++ main.py
@@ -1,23 +1,9 @@
 from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
 
 from src.routes.StudentRoutes import router as student_router
 from src.routes.RecruiterRoutes import router as recruiter_router
 
 app = FastAPI()
-
-# CORS setup
-origins = [
-    "https://internhub-backend-fqmw.onrender.com",
-]
-
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=origins,
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
 
 # Student routes
 app.include_router(
